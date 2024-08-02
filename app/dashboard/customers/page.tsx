1b37524acdc854954993b30c9d45e1c5b0efae73
anastasiiayesypenko <43060668+anastasiiayesypenko@users.noreply.github.com>
2024-08-02 17:30:03 +0300

    finish main course layout

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,53 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
+import { TableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCustomersPages } from '@/app/lib/data';
+import Table from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+const skeletonHeaderColumns = [
+  { title: 'Name', className: 'px-4 py-5 font-medium sm:pl-6' },
+  { title: 'Email', className: 'px-3 py-5 font-medium' },
+  { title: 'Total Invoices', className: 'px-3 py-5 font-medium' },
+  { title: 'Total Pending', className: 'px-3 py-5 font-medium' },
+  { title: 'Total Paid', className: 'px-4 py-5 font-medium' },
+];
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Suspense
+        key={query + currentPage}
+        fallback={<TableSkeleton headerColumns={skeletonHeaderColumns} />}
+      >
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
